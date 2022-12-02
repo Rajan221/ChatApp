3238996d0c7d5cefebebf22320ebff2d5b55936f
Rajan221 <rajansuper221@gmail.com>
2022-12-02 20:53:21 +0545

    Login Completed

--- chat/views.py
+++ chat/views.py
@@ -5,17 +5,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-
-
-
-
-
+# for signup
 
 def signUp(request):
+    
     return render(request, "login/signup.html")
 
 def login(request):
-    return render(request, "login/login.html")
+    if request.method =="POST":
+        return render(request, "home.html")
+        
+    else:
+        return render(request, "login/login.html")
 
 def home(request):
     return render(request, "home.html")
