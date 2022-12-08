81940695760ce187f8899e76ae7a59150cf17953
Rajan221 <rajansuper221@gmail.com>
2022-12-09 01:40:16 +0545

    backend for login and signup

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class userDetails(models.Model):
+    
+    firstName = models.CharField(max_length=100)
+    lastName= models.CharField(max_length=100)
+    email = models.EmailField( max_length=254)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.firstName
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from chat.models import userDetails
 
 
 # Create your views here.
@@ -8,15 +8,67 @@ from django.http import HttpResponse
 # for signup
 
 def signUp(request):
-    
-    return render(request, "login/signup.html")
+    if request.method == "POST":
+        firstName = request.POST.get("fname")
+        lastName = request.POST.get("lname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
 
+        if userDetails.objects.filter(email=email, password=password):
+                return HttpResponse("User already exists")
+        else:
+            userAuth = userDetails(firstName=firstName, lastName = lastName, email=email, password = password)
+            userAuth.save()
+            return render(request, "login/login.html")
+            
+    
+
+    else:
+        return render(request, "login/signup.html")
+
+
+#login logic
 def login(request):
-    if request.method =="POST":
-        return render(request, "home.html")
-        
+
+    if request.method== "POST":
+        return userAuth(request)
+
     else:
         return render(request, "login/login.html")
 
-def home(request):
-    return render(request, "home.html")
+def home(request, user):
+    contex={"user": user.firstName}
+    return render(request, "home.html", contex)
+
+
+
+
+
+
+# *****non page logics************
+#To Check if the user is registered
+
+
+    
+#Authorization function
+def userAuth(request):     
+    email=  request.POST.get("email")
+    password = request.POST.get("password")
+
+    return userCheck(email, password, request)
+
+def userCheck(email, password, request):
+    try: 
+        user =userDetails.objects.get(email=email, password=password)
+    except: 
+        user= False
+    
+    
+    if user:
+        
+        return home(request, user)
+    else:
+        return HttpResponse("user not found")
+
+
+       
